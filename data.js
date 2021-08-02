d031579249ce17446b5aef43beae9722d427440e
vishuftw <85093736+vishuftw@users.noreply.github.com>
2021-08-02 20:41:22 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://zishu:vishuvanar68@ltc.ufen0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'1932443873:AAELcS91p3APIZKlNd2MHrf8gizoHwvcb-U',//bot token
+    
+    bot_name:'LTCBoom_bot',//bot username without @
+    
+    bot_admin:1891485685,// admin telegram id
+    
+    channelsList : ['@cyptoftw69' , '@FTWPayouts'],// channels list
+  
+    
+    reffer_bonus:1000,// refferal bonus amount
+    
+    min_wd:0.000005,//min withdrawal amount
+    
+    daily_bonus:0.000001,// daily bonus amount
+    
+    currency:'Litecoin', //bot cuurency
+    
+    cb_api_key:'6egAFBb9t8Ck3UXw',//coinbase api key
+    
+    cb_api_secret:'EmIAnUWn59gai6CmJubcMiq3LSsPgm9U',// coinbase secret key
+    
+    cb_account_id:'a0a96c12-1fc7-53f8-bbb6-99b6fbd48a59',// counbase account id for the bot currency
+    
+    payment_channel:'@FTWPayouts'// payment channel username
+    
+    }
